8aa88ee14f37b15f7829efa84f59ead1896e339b
Raman Mohammed <a21rammo@outlook.com>
2022-06-13 21:26:37 +0200

    i dont remember

--- src/main/java/com/example/oktademo/OktaDemoApplication.java
+++ src/main/java/com/example/oktademo/OktaDemoApplication.java
@@ -2,12 +2,26 @@ package com.example.oktademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 public class OktaDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OktaDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OktaDemoApplication.class, args);
+    }
+
+    @RestController
+    static class SimpleController {
+        @GetMapping
+        String welcome(@AuthenticationPrincipal OidcUser principal) {
+            return "Hello" + principal.getName();
+        }
+    }
 
 }
